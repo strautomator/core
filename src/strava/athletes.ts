045cc1f08a478798ff05851fde947fbe8e693ce1
Igor Ramadas <igor@devv.com>
2021-05-12 14:40:49 +0200

    Increased extra hourly FTP weight to 103.5%.

--- src/strava/athletes.ts
+++ src/strava/athletes.ts
@@ -143,14 +143,14 @@ export class StravaAthletes {
                 let power: number
 
                 // FTP ranges from 95% to 100% from 20 minutes to 1 hour, and then
-                // 103% for each extra hour of activity time.
+                // 103.5% for each extra hour of activity time.
                 if (totalTime <= 3600) {
                     const perc = ((3600 - totalTime) / 60 / 8) * 0.01
                     power = Math.round(watts * (1 - perc))
                 } else {
                     const extraHours = Math.floor(totalTime / 3600) - 1
-                    const fraction = 1 + 0.025 * ((totalTime % 3600) / 60 / 60)
-                    const factor = 1.025 ** extraHours * fraction
+                    const fraction = 1 + 0.035 * ((totalTime % 3600) / 60 / 60)
+                    const factor = 1.035 ** extraHours * fraction
                     power = watts * factor
                 }
 
