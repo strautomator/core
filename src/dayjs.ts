bc39377bb00b144d25555b5e0af6a7648b679c86
Igor Ramadas <igor@devv.com>
2021-05-27 12:05:23 +0200

    Added missing weekOfYear plugin.

--- src/dayjs.ts
+++ src/dayjs.ts
@@ -4,8 +4,9 @@ import dayjs from "dayjs"
 import dayjsAdvancedFormat from "dayjs/plugin/advancedFormat"
 import dayjsLocalizedFormat from "dayjs/plugin/localizedFormat"
 import dayjsUTC from "dayjs/plugin/utc"
-import dayjsDayOfYear from "dayjs/plugin/dayOfYear"
 import dayjsWeekYear from "dayjs/plugin/weekYear"
+import dayjsWeekOfYear from "dayjs/plugin/weekOfYear"
+import dayjsDayOfYear from "dayjs/plugin/dayOfYear"
 import dayjsDuration from "dayjs/plugin/duration"
 import dayjsRelativeTime from "dayjs/plugin/relativeTime"
 
@@ -13,8 +14,9 @@ import dayjsRelativeTime from "dayjs/plugin/relativeTime"
 dayjs.extend(dayjsAdvancedFormat)
 dayjs.extend(dayjsLocalizedFormat)
 dayjs.extend(dayjsUTC)
-dayjs.extend(dayjsDayOfYear)
 dayjs.extend(dayjsWeekYear)
+dayjs.extend(dayjsWeekOfYear)
+dayjs.extend(dayjsDayOfYear)
 dayjs.extend(dayjsDuration)
 dayjs.extend(dayjsRelativeTime)
 
