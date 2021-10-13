3bfeb7c08f8cb9ba57cce3cea73d4625bd8fab93
Igor Ramadas <igor@devv.com>
2021-10-14 00:18:22 +0200

    Initial code to get interesting activities posted on Twitter.

--- src/twitter/index.ts
+++ src/twitter/index.ts
@@ -1,6 +1,12 @@
 // Strautomator Core: Twitter
 
+import {StravaActivity} from "../strava/types"
+import {UserData} from "../users/types"
+import * as messageTemplates from "./messages"
+import eventManager from "../eventmanager"
+import _ = require("lodash")
 import TwitterLite from "twitter-lite"
+import jaul = require("jaul")
 import logger = require("anyhow")
 const settings = require("setmeup").settings
 
@@ -60,12 +66,61 @@ export class Twitter {
             } else {
                 this.getAccountDetails()
             }
+
+            // Strava events.
+            eventManager.on("Strava.processActivity", this.onStravaActivity)
         } catch (ex) {
             logger.error("Twitter.init", ex)
         }
     }
 
-    // POSTING
+    /**
+     * Post interesting activities to Twitter. A max of 10% of activities from users that opted
+     * for Twitter sharing will be processed here.
+     * @param user The activity owner.
+     * @param activity The activity data.
+     */
+    private onStravaActivity = async (user: UserData, activity: StravaActivity): Promise<void> => {
+        try {
+            if (!user.preferences || !user.preferences.twitterShare) return
+            if (Math.random() < 0.9) return
+
+            // Parameters to decide if the ride was "impressive" or not.
+            const imperial: boolean = user.profile.units == "imperial"
+            const rideDistance: number = imperial ? 130 : 200
+            const rideSpeed: number = imperial ? 26 : 42
+            const runDistance: number = imperial ? 26 : 42
+            let possibleMessages: string[] = null
+
+            // Rides.
+            if (activity.type == "Ride") {
+                if (activity.distance > rideDistance) {
+                    possibleMessages = messageTemplates.RideLongDistance
+                } else if (activity.speedAvg > rideSpeed) {
+                    possibleMessages = messageTemplates.RideFast
+                }
+            }
+
+            // Runs.
+            else if (activity.type == "Run") {
+                if (activity.distance > runDistance) {
+                    possibleMessages = messageTemplates.RunLongDistance
+                }
+            }
+
+            // Nothing interesting to post?
+            if (!possibleMessages) {
+                logger.info("Twitter.onStravaActivity", `User ${user.id} ${user.displayName}`, `Activity ${activity.id}`, "Not interesting, won't post")
+                return
+            }
+
+            await this.postActivity(user, activity, _.sample(possibleMessages))
+        } catch (ex) {
+            logger.error("Twitter.onStravaActivity", `User ${user.id} ${user.displayName}`, `Activity ${activity.id}`, ex)
+        }
+    }
+
+    // INTERNAL METHODS
     // --------------------------------------------------------------------------
 
     /**
@@ -86,15 +141,27 @@ export class Twitter {
     /**
      * Post a message to Twitter.
      */
-    post = async (status: string): Promise<void> => {
+    postStatus = async (status: string): Promise<void> => {
         try {
-            await this.client.post("statuses/update", {
-                status: status
-            })
-
-            logger.info("Twitter.post", status)
+            await this.client.post("statuses/update", {status: status})
+            logger.info("Twitter.postStatus", status)
         } catch (ex) {
-            logger.error("Twitter.post", status, ex)
+            logger.error("Twitter.postStatus", status, ex)
+        }
+    }
+
+    /**
+     * Post about a user activity on Twitter. This is a shortcut to
+     * postStatus() with the activity details as the status message.
+     */
+    postActivity = async (user: UserData, activity: StravaActivity, message: string): Promise<void> => {
+        try {
+            message = jaul.data.replaceTags(message, {user: user.displayName})
+            message = jaul.data.replaceTags(message, activity)
+
+            await this.postStatus(`${message} https://strava.com/activities/${activity.id}`)
+        } catch (ex) {
+            logger.error("Twitter.postActivity", `User ${user.id} ${user.displayName}`, `Activity ${activity.id}`, ex)
         }
     }
 }
---
+++ src/twitter/messages.ts
@@ -0,0 +1,16 @@
+// Strautomator Core: Twitter messages
+
+/**
+ * Long distance rides.
+ */
+export const RideLongDistance = ["A massive ${distance} ride by ${user}! Well done!", "An impressive ${distance} ride by ${user} just updated with Strautomator!", ""]
+
+/**
+ * Fast rides.
+ */
+export const RideFast = ["Impressively fast ride by ${user}! ${speedAvg} average speed 😲", "Rocket 🚀 Check that ${speedAvg} ride by ${user}."]
+
+/**
+ * Long distance runs.
+ */
+export const RunLongDistance = ["What a massive ${distance} run by ${user}!", "Impressive ${distance} run by ${user} just automated with Strautomator!", ""]
