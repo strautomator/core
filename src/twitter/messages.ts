eff6bfedff79a0d4cc4e37c16c86b3329b0a3f72
Igor Ramadas <igor@devv.com>
2021-10-14 11:21:52 +0200

    No auto-formatting on Twitter message definitions.

--- src/twitter/messages.ts
+++ src/twitter/messages.ts
@@ -1,16 +1,31 @@
 // Strautomator Core: Twitter messages
 
+// prettier-ignore
 /**
  * Long distance rides.
  */
-export const RideLongDistance = ["A massive ${distance} ride by ${user}! Well done!", "An impressive ${distance} ride by ${user} just updated with Strautomator!", ""]
+export const RideLongDistance = [
+    "A massive ${distance} ride by ${user}! Well done!",
+    "An impressive ${distance} ride by ${user}, just updated with Strautomator!",
+    "A long day in the saddle for ${user}, covering north of ${distance} today."
+]
 
+// prettier-ignore
 /**
  * Fast rides.
  */
-export const RideFast = ["Impressively fast ride by ${user}! ${speedAvg} average speed 😲", "Rocket 🚀 Check that ${speedAvg} ride by ${user}."]
+export const RideFast = [
+    "Impressively fast ride by ${user}! ${speedAvg} average speed 😲",
+    "Rocket speed 🚀 Check that ${speedAvg} ride by ${user}.",
+    "Work hard, cycle harder! A fast ${speedAvg} ride by ${user} today."
+]
 
+// prettier-ignore
 /**
  * Long distance runs.
  */
-export const RunLongDistance = ["What a massive ${distance} run by ${user}!", "Impressive ${distance} run by ${user} just automated with Strautomator!", ""]
+export const RunLongDistance = [
+    "A solid ${distance} run by ${user} automated with Strautomator. Well done!",
+    "Impressive ${distance} run by ${user} just automated with Strautomator!",
+    "Keep on running, ${user}! And well done for your ${distance} run today 🙂"
+]
