b6dad908a4261a20979e711140f6d24da242a32b
Igor Ramadas <igor@devv.com>
2021-11-10 16:13:49 +0100

    A few additions to the activity "fortune" quotes and conditions.

--- src/fortune.ts
+++ src/fortune.ts
@@ -3,6 +3,7 @@
 import {StravaActivity, StravaSport} from "./strava/types"
 import {UserData} from "./users/types"
 import _ = require("lodash")
+import logger = require("anyhow")
 
 /**
  * Random funny quotes.
@@ -15,25 +16,24 @@ export const fortuneCookies: string[] = [
     "Roses are red, my name is not Dave, this makes no sense, microwave.",
     "Yesterday I did nothing and today I'm finishing what I did yesterday.",
     "Doing nothing is hard, you never know when you're done.",
-    "Common sense is like deodorant. The people who need it most never use it.",
     "If I’m not back in five minutes, just wait longer.",
     "Why do they call it rush hour when nothing moves?",
     "Get your facts first, then you can distort them as you please.",
     "What's another word for Thesaurus?",
-    "If you want a guarantee, buy a toaster.",
     "I can resist everything except temptation.",
     "Weather forecast for tonight: dark.",
     "Cure for an obsession: get another one.",
     "One advantage of talking to yourself is that you know at least somebody's listening.",
-    "Roses are red, violets are blue, I'm schizophrenic, and so am I.",
     "It never gets easier, you just go faster.",
     "Beyond pain there is a whole universe of more pain.",
-    "You never have the wind with you - either it is against you or you’re having a good day.",
+    "You never have the wind with you - either it's against you or you’re having a good day.",
     "It is the unknown around the corner that turns my wheels.",
     "I’d like to help you out. Which way did you come in?",
     "I doubt, therefore I might be.",
     "Constipated people don’t give a crap.",
-    "All generalizations are false."
+    "All generalizations are false.",
+    "Hello world!",
+    "I tried to set my password to 'beef stew' but it wasn't stroganoff."
 ]
 
 /**
@@ -43,24 +43,27 @@ export const fortuneCookies: string[] = [
  */
 export const getActivityFortune = (user: UserData, activity: StravaActivity): string => {
     const imperial = user.profile.units == "imperial"
-    const prefixes = ["", "delightful", "amazing", "", "great", "just your regular", "", "crazy", "superb", "", "magnificent", "marvellous", ""]
+    const prefixes = ["", "delightful", "amazing", "great", "", "just your regular", "crazy", "superb", "", "magnificent", "marvellous", "exotic", ""]
     const names = []
     const uniqueNames = []
 
-    // Virtual rides prefix.
-    if (activity.type == StravaSport.VirtualRide) {
-        prefixes.push("virtual")
-        prefixes.unshift("virtual")
-    }
+    // Rounded activity properties.
+    const distanceR = Math.round(activity.distance)
+    const speedAvgR = Math.round(activity.speedAvg)
+    const elevationGainR = Math.round(activity.elevationGain)
 
-    // Commute prefix.
-    if (activity.commute) {
-        prefixes.push("commute: ")
-        prefixes.push("yet another commute: ")
+    // Virtual ride prefix.
+    if (activity.type == StravaSport.VirtualRide) {
+        prefixes.push("virtual:")
+        prefixes.unshift("virtual:")
+    }
+    if (activity.trainer) {
+        prefixes.push("turbo trainer:")
+        prefixes.unshift("indoor:")
     }
 
     // Cycling.
-    if (activity.type == StravaSport.Ride || activity.type == StravaSport.VirtualRide) {
+    if (activity.type == StravaSport.Ride || activity.type == StravaSport.VirtualRide || activity.type == StravaSport.EBikeRide) {
         if (activity.distance >= 500) {
             uniqueNames.push("almost a lap around the world")
             uniqueNames.push("short and easy that was")
@@ -72,16 +75,21 @@ export const getActivityFortune = (user: UserData, activity: StravaActivity): st
         } else if (activity.distance > 99 && activity.distance < 100) {
             names.push("almost-a-century ride")
             names.push("and so close to 3 digits")
-        } else if ((imperial && activity.distance < 6) || activity.distance < 10) {
+        } else if ((imperial && distanceR == 26) || distanceR == 42) {
+            uniqueNames.push("marathon on two wheels")
+            uniqueNames.push("maratho... on a bike")
+        } else if (((imperial && activity.distance < 6) || activity.distance) < 10 && activity.distance > 0) {
             names.push("and short, too short of a ride")
-            names.push("short ride")
+            names.push("short, very short ride")
+            names.push("shorty")
         }
 
         if ((imperial && activity.speedAvg > 31) || activity.speedAvg >= 50) {
             uniqueNames.push("fast and furious")
             uniqueNames.push("shut up legs")
+            uniqueNames.push("lightspeed")
             uniqueNames.push("push push push")
-        } else if ((imperial && activity.speedAvg < 6) || activity.speedAvg < 10) {
+        } else if (((imperial && activity.speedAvg < 5) || activity.speedAvg < 8) && activity.speedAvg > 0) {
             uniqueNames.push("slow does it")
             uniqueNames.push("who's in a hurry?")
         }
@@ -89,18 +97,29 @@ export const getActivityFortune = (user: UserData, activity: StravaActivity): st
         if (activity.wattsMax > 1600 || activity.wattsAvg > 400) {
             uniqueNames.push("rocket propelled")
             uniqueNames.push("shut up legs")
+        } else if (activity.wattsAvg < 100 && activity.wattsAvg > 0) {
+            names.push("easy does it")
+            names.push("soft pedalling")
+        }
+
+        if (activity.distance > 0 && activity.elevationGain > 0 && activity.climbingRatio <= 0.1) {
+            names.push("flatland tour")
+            names.push("ride through some massive hills")
         }
     }
 
     // Running.
-    else if (activity.type == StravaSport.Run) {
+    else if (activity.type == StravaSport.Run || activity.type == StravaSport.Walk) {
         if ((imperial && activity.distance >= 52) || activity.distance >= 84) {
             uniqueNames.push("when a marathon is not enough")
             uniqueNames.push("double marathon")
         } else if ((imperial && activity.distance >= 26) || activity.distance >= 42) {
             names.push("marathon")
-        } else if ((imperial && activity.distance < 3) || activity.distance < 5) {
-            names.push("short run")
+        } else if (distanceR == 10) {
+            names.push("10K")
+            names.push("10K or 6 miles?")
+        } else if (((imperial && activity.distance < 3) || activity.distance < 5) && activity.distance > 0) {
+            names.push("super short run")
             names.push("mini workout")
         }
     }
@@ -110,15 +129,19 @@ export const getActivityFortune = (user: UserData, activity: StravaActivity): st
         uniqueNames.push("everesting")
         uniqueNames.push("the sky is the limit")
         uniqueNames.push("don’t buy upgrades, ride up grades")
+        uniqueNames.push("don’t upgrade, go up grades")
     } else if ((!imperial && activity.elevationGain > 2500) || activity.elevationGain > 9000) {
         names.push("higher and higher")
-        names.push("don’t upgrade, ride up grades")
+        names.push("roller coaster")
+        names.push("way up to the sky")
     }
 
     // Ultra long workouts or short workouts.
     if (activity.movingTime > 86400) {
         uniqueNames.push("keep going, never stop")
         uniqueNames.push("that was a long day")
+    } else if (activity.movingTime > 28800) {
+        uniqueNames.push("call it a work day")
     } else if (activity.distance < 1) {
         uniqueNames.push("now that was quick")
         uniqueNames.push("training for the IRONMAN")
@@ -130,7 +153,8 @@ export const getActivityFortune = (user: UserData, activity: StravaActivity): st
         uniqueNames.push("a week's worth of calories")
         uniqueNames.push("energy galore")
     } else if (activity.calories > 5000) {
-        names.push("calories, calories")
+        names.push("caloric extermination")
+        names.push("caloric workout")
     }
 
     // High heart rate.
@@ -146,16 +170,38 @@ export const getActivityFortune = (user: UserData, activity: StravaActivity): st
         names.push("knitting machine")
     }
 
+    // Matching properties.
+    if (distanceR == speedAvgR) {
+        uniqueNames.push(`${distanceR} / ${speedAvgR}`)
+    }
+    if (elevationGainR == 1234) {
+        uniqueNames.push("one two three four")
+    }
+
+    // Commutes.
+    if (activity.commute) {
+        if (names.length > 0) {
+            prefixes.push("commute:")
+            prefixes.unshift("yet another commute:")
+        } else {
+            names.push("commuting around")
+            names.push("commute galore")
+        }
+    }
+
     // Build resulting string.
-    // If unique names were set, use one with 95% chance.
-    // If regulr names were set, use it with a 75% chance.
+    // If unique names were set, use one with 90% chance.
+    // If regulr names were set, use it with a 70% chance.
     // Everything else, use a funny quote.
     let result: string
-    if (uniqueNames.length > 0 && Math.random() < 0.95) {
+    if (uniqueNames.length > 0 && Math.random() < 0.91) {
         result = _.sample(uniqueNames)
-    } else if (names.length > 0 && Math.random() < 0.75) {
+    } else if (names.length > 0 && Math.random() < 0.71) {
         result = `${_.sample(prefixes)} ${_.sample(names)}`.trim()
     }
 
-    return result ? result.charAt(0).toUpperCase() + result.slice(1) : _.sample(fortuneCookies)
+    result = result ? result.charAt(0).toUpperCase() + result.slice(1) : _.sample(fortuneCookies)
+    logger.info("Fortune.getActivityFortune", `Activity ${activity.id}`, result)
+
+    return result
 }
