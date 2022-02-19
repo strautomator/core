6b84d9ea7c03a39663a3ba9b12aec768d2f77fb2
Igor Ramadas <igor@devv.com>
2022-02-19 19:57:59 +0100

    CachedCalendar interface not needed any longer.

--- src/calendar/types.ts
+++ src/calendar/types.ts
@@ -1,22 +1,6 @@
 // Strautomator Core: Calendar types
 
 /**
- * Cached calendar stored on the database.
- */
-export interface CachedCalendar {
-    /** Cached calendar ID. */
-    id: string
-    /** User ID. */
-    userId: string
-    /** Date when calendar was last updated. */
-    dateUpdated: Date
-    /** The calendar data (as .ics string). */
-    data?: string
-    /** If calendar is too long, data is stored as shards instead. */
-    shards?: any[]
-}
-
-/**
  * Options used to generate activity calendars.
  */
 export interface CalendarOptions {
