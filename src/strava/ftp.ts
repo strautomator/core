e7037431fa6e814478ef0795bf66d0570572c2ee
Igor Ramadas <igor@devv.com>
2022-06-15 19:42:55 +0200

    Increased the current FTP weight from 25% to 35% when downgrading the user's FTP.

--- src/strava/ftp.ts
+++ src/strava/ftp.ts
@@ -104,7 +104,7 @@ export class StravaFtp {
             // Otherwise get the weighted or current value itself, whatever is the lowest.
             if (currentWatts && currentWatts > maxWatts) {
                 const maxWattsWeight = [maxWatts, 1]
-                const currentWattsWeight = [currentWatts, 1.25]
+                const currentWattsWeight = [currentWatts, 1.35]
                 const ftpWeights = [maxWattsWeight, currentWattsWeight]
                 const [ftpTotalSum, ftpWeightSum] = ftpWeights.reduce(([valueSum, weightSum], [value, weight]) => [valueSum + value * weight, weightSum + weight], [0, 0])
                 ftpWatts = ftpTotalSum / ftpWeightSum
