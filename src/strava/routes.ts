75594d898e4c29ae95270667cafe115e73b25318
Igor Ramadas <igor@devv.com>
2022-08-08 18:47:30 +0300

    New methods to get and zip route GPX files.

--- src/strava/routes.ts
+++ src/strava/routes.ts
@@ -5,6 +5,7 @@ import {toStravaRoute} from "./utils"
 import {UserData} from "../users/types"
 import api from "./api"
 import logger = require("anyhow")
+import JSZip = require("jszip")
 
 /**
  * Strava routes manager.
@@ -36,7 +37,62 @@ export class StravaRoutes {
             logger.info("Strava.getRoute", `User ${user.id} ${user.displayName}`, `Route ${id}: ${route.name}`)
             return route
         } catch (ex) {
-            logger.warn("Strava.getRoute", `User ${user.id} ${user.displayName}`, id, ex)
+            logger.warn("Strava.getRoute", `User ${user.id} ${user.displayName}`, `Route ${id}`, ex)
+            return null
+        }
+    }
+
+    /**
+     * Get the GPX representation of the specified route.
+     * @param user User data.
+     * @param id The route ID.
+     */
+    getGPX = async (user: UserData, id: string): Promise<any> => {
+        try {
+            const data = await api.get(user.stravaTokens, `routes/${id}/export_gpx`)
+
+            if (!data) {
+                logger.info("Strava.getGPX", `User ${user.id} ${user.displayName}`, `Route ${id}: no GPX`)
+                return null
+            }
+
+            logger.info("Strava.getGPX", `User ${user.id} ${user.displayName}`, `Route ${id}: length ${data.toString().length}`)
+            return data
+        } catch (ex) {
+            logger.error("Strava.getGPX", `User ${user.id} ${user.displayName}`, `Route ${id}`, ex)
+            throw ex
+        }
+    }
+
+    /**
+     * Generate a ZIP file with the specified GPX routes.
+     * @param user User data.
+     * @param ids The route IDs as an array.
+     */
+    zipGPX = async (user: UserData, routeIds: string[]): Promise<NodeJS.ReadableStream> => {
+        try {
+            if (!routeIds || routeIds.length == 0) {
+                routeIds = []
+                throw new Error("Missing route IDs")
+            }
+
+            if (!user.isPro) {
+                throw new Error("GPX downloads are available to PRO users only")
+            }
+
+            // Add the individual routes to the ZIP file.
+            const zip = new JSZip()
+            for (let id of routeIds) {
+                const gpx = await this.getGPX(user, id)
+                zip.file(`${id}.gpx`, gpx)
+            }
+
+            const result = zip.generateNodeStream({type: "nodebuffer", streamFiles: true})
+            logger.info("Strava.zipGPX", `User ${user.id} ${user.displayName}`, `Routes: ${routeIds.join(", ")}`)
+
+            return result
+        } catch (ex) {
+            logger.error("Strava.zipGPX", `User ${user.id} ${user.displayName}`, `Routes: ${routeIds.join(", ")}`, ex)
             throw ex
         }
     }
