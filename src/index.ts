d2642ce0faeeb6656379bfafca1c9543c05d9f0b
Igor Ramadas <igor@devv.com>
2023-05-22 19:04:23 +0200

    Process queued activities every 2 minutes on dev.

--- src/index.ts
+++ src/index.ts
@@ -224,11 +224,11 @@ export const startup = async (quickStart?: boolean) => {
         try {
             logger.warn("Strautomator.startup", "Setting up cron jobs directly")
 
-            // Process queued activities every 10 minutes.
+            // Process queued activities every 2 minutes.
             const processQueuedActivities = async () => {
                 await strava.activityProcessing.processQueuedActivities()
             }
-            setInterval(processQueuedActivities, 1000 * 60 * 10)
+            setInterval(processQueuedActivities, 1000 * 60 * 2)
 
             // Cleanup old queued activities every hour.
             const cleanupQueuedActivities = async () => {
