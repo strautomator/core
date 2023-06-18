e6adb909d1171a16f33cec7662b140092daa9fed
Igor Ramadas <igor@devv.com>
2023-06-18 23:09:31 +0200

    Variable "break per few hours" time depending on user fitness, when calculating the route estimated total time.

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -36,15 +36,25 @@ export class Routes {
                 [StravaFitnessLevel.Pro]: 0.99,
                 [StravaFitnessLevel.Elite]: 0.97
             }
+            const breakSplits = {
+                [StravaFitnessLevel.Untrained]: 10080,
+                [StravaFitnessLevel.Average]: 10800,
+                [StravaFitnessLevel.Athletic]: 11160,
+                [StravaFitnessLevel.Pro]: 11340,
+                [StravaFitnessLevel.Elite]: 11520
+            }
 
             // No total time set? Estimate it now, using different multipliers depending on the user's fitness level.
+            // Example: total time for average users will be 6% added to the moving time, plus a break of 40min
+            // for every 3 hours of riding (10800) seconds.
             if (!route.totalTime) {
                 const multiplier = multipliers[user.fitnessLevel || StravaFitnessLevel.Average]
+                const breakSplit = breakSplits[user.fitnessLevel || StravaFitnessLevel.Average]
                 const secondsEstimated = route.movingTime * multiplier
-                const secondsExtraBreaks = Math.floor(route.movingTime / 10800) * settings.routes.extraTimePer3Hours
+                const secondsExtraBreaks = Math.floor(route.movingTime / breakSplit) * settings.routes.breakTime
                 const duration = dayjs.duration(secondsEstimated + secondsExtraBreaks, "seconds")
                 const toQuarter = 15 - (duration.minutes() % 15)
-                route.totalTime = duration.add(toQuarter, "minutes").asSeconds()
+                route.totalTime = Math.round(duration.add(toQuarter, "minutes").asSeconds())
             }
         } catch (ex) {
             logger.error("Routes.estimateTotalTime", logHelper.user(user), route.id, ex)
