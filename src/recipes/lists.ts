50e5bbc6d58f411f75f704b5ba6d8d68ccb982f9
Igor Ramadas <igor@devv.com>
2023-11-16 18:27:57 +0100

    CO2 saved, not Saved.

--- src/recipes/lists.ts
+++ src/recipes/lists.ts
@@ -101,7 +101,7 @@ export const recipePropertyList = [
     {value: "gear", text: "Gear", type: "gear", operators: recipeOperatorList.is},
     {value: "device", text: "Device or app", type: "text", operators: recipeOperatorList.text},
     {value: "temperature", text: "Device temperature", type: "anyNumber", operators: recipeOperatorList.number, suffix: "°C", fSuffix: "°F"},
-    {value: "co2Saved", text: "CO2 Saved", type: "number", operators: recipeOperatorList.number, suffix: "kg"},
+    {value: "co2Saved", text: "CO2 saved", type: "number", operators: recipeOperatorList.number, suffix: "kg"},
     {value: "trainer", text: "Using a trainer machine", type: "boolean", operators: recipeOperatorList.is},
     {value: "manual", text: "Created manually", type: "boolean", operators: recipeOperatorList.is},
     {value: "flagged", text: "Flagged", type: "boolean", operators: recipeOperatorList.is},
