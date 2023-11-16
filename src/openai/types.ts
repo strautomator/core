c8e321725c12f2ca2aa541f1cefb48bf8215bcd6
Igor Ramadas <igor@devv.com>
2023-11-16 18:09:51 +0100

    New ChatGptResponse interface.

---
+++ src/openai/types.ts
@@ -0,0 +1,11 @@
+// Strautomator Core: OpenAI types
+
+/**
+ * ChatGPT prompt and response data.
+ */
+export interface ChatGptResponse {
+    /** Prompt sent to ChatFPT. */
+    prompt: string
+    /** Response from ChatGPT. */
+    response: string
+}
