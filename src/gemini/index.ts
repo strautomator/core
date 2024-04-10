6a5e1252f8aa24ce9d6d80a75a8b9c8c7a5437ea
Igor Ramadas <igor@devv.com>
2024-04-10 22:33:38 +0200

    Gemini API calls updated to reflect changes to the Vertex library.

--- src/gemini/index.ts
+++ src/gemini/index.ts
@@ -75,14 +75,14 @@ export class Gemini implements AiProvider {
             // Here we go!
             const reqOptions: GenerateContentRequest = {
                 contents: [{role: "user", parts: parts}],
-                safety_settings: [
+                safetySettings: [
                     {category: HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT, threshold: HarmBlockThreshold.BLOCK_ONLY_HIGH},
                     {category: HarmCategory.HARM_CATEGORY_HARASSMENT, threshold: HarmBlockThreshold.BLOCK_ONLY_HIGH},
                     {category: HarmCategory.HARM_CATEGORY_HATE_SPEECH, threshold: HarmBlockThreshold.BLOCK_ONLY_HIGH},
                     {category: HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT, threshold: HarmBlockThreshold.BLOCK_ONLY_HIGH}
                 ],
-                generation_config: {
-                    max_output_tokens: maxTokens
+                generationConfig: {
+                    maxOutputTokens: maxTokens
                 }
             }
             const jobId = `${activity.id}-${prompt.length}-${maxTokens}`
