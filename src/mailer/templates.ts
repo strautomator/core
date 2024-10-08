f2ae7de1ce9b6011acf2ba2c1eaf4b31cc76667d
Igor Ramadas <igor@devv.com>
2024-10-08 23:21:16 +0200

    Removed PaddleMigration template.

--- src/mailer/templates.ts
+++ src/mailer/templates.ts
@@ -155,32 +155,5 @@ export const EmailTemplates = {
             "<p>" +
             "<i>If you haven't registered an email with Strautomator, please ignore this email.</i>" +
             "</p>"
-    },
-
-    // User must confirm the email address.
-    PaddleMigration: {
-        subject: "Migration of your PRO subscription",
-        body:
-            "<p>" +
-            "Hi ${userName}!<br><br>" +
-            "Strautomator has a new payment provider, Paddle.com! It supports not only PayPal, but also all major credit cards, Google Pay, Apple Pay, iDEAL and Bancontact." +
-            "</p>" +
-            "<p>" +
-            "Your Strautomator PRO subscription is scheduled to be billed via PayPal in a few days. I kindly ask that you migrate your subscription to Paddle, if possible.<br><br>As a 'thank you' gesture, you'll get a discount code, bringing the initial subscription price down from ${currentPrice} ${currency} to ${discountPrice} ${currency}.<br>Please note that we have a limited amount of discount codes. <i>First come, first serve.</i>" +
-            "</p>" +
-            "<p>" +
-            "To get more details and proceed with the migration, please go to " +
-            '<a href="${appUrl}billing/paddlemigration" title="Click to get more details about the Migration to Paddle">${appUrl}billing/paddlemigration</a>.' +
-            "</p>" +
-            "<p>" +
-            "If you don't want to do it now, no worries, existing PayPal subscriptions will be valid at least until December 2025." +
-            "</p>" +
-            "<p>" +
-            "Need any more information? Simply reply to this email and I'll get back to you." +
-            "</p>" +
-            "<p>" +
-            "Kind regards,<br><br>" +
-            "Igor Ramadas" +
-            "</p>"
     }
 }
