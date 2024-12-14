615c146ce9ac23a3d5383dd9cef7baa781c26f20
Igor Ramadas <igor@devv.com>
2024-12-14 22:14:55 +0100

    Fully revamped FIT parser, now based on the official Garmin SDK.

--- src/fitparser/index.ts
+++ src/fitparser/index.ts
@@ -1,9 +1,8 @@
 // Strautomator Core: FIT Parser
 // Largely based on https://github.com/jimmykane/fit-parser
 
-import {FIT} from "./fit"
+import {Decoder, Stream} from "./sdk"
 import {FitFileActivity} from "./types"
-import {getArrayBuffer, calculateCRC, readRecord} from "./binary"
 import {DatabaseSearchOptions} from "../database/types"
 import {StravaActivity, StravaProcessedActivity} from "../strava/types"
 import {UserData} from "../users/types"
@@ -30,203 +29,45 @@ export class FitParser {
      * @param user The user.
      * @param fitFileActivity The FIT file activity to have the data appended to.
      * @param rawData The FIT raw data.
-     * @param options Optional parsing options.
      */
-    async parse(user: UserData, fitFileActivity: FitFileActivity, rawData: any, options?: any) {
-        if (!options) {
-            options = {
-                force: true,
-                speedUnit: "m/s",
-                lengthUnit: "m",
-                temperatureUnit: "celsius",
-                elapsedRecordField: false
-            }
+    async parse(user: UserData, fitFileActivity: FitFileActivity, rawData: any) {
+        const stream = Stream.fromByteArray(rawData)
+        if (!Decoder.isFIT(stream)) {
+            throw new Error("Not a valid FIT file")
         }
 
-        const blob = new Uint8Array(getArrayBuffer(rawData))
-
-        if (blob.length < 12) {
-            throw new Error("File to small to be a FIT file")
+        const decoder = new Decoder(stream)
+        if (!decoder.checkIntegrity()) {
+            throw new Error("FIT file integrity check failed")
         }
 
-        const headerLength = blob[0]
-        if (headerLength !== 14 && headerLength !== 12) {
-            throw new Error("Incorrect header size")
+        const {messages, errors} = decoder.read()
+        if (errors.length > 0) {
+            const logErrors = errors.map((e) => e.message || e.toSting()).join(", ")
+            logger.warn("FitParser.parse", logHelper.user(user), fitFileActivity.id, `Parsing errors: ${logErrors}`)
         }
-
-        let fileTypeString = ""
-        for (let i = 8; i < 12; i++) {
-            fileTypeString += String.fromCharCode(blob[i])
-        }
-        if (fileTypeString !== ".FIT") {
-            throw new Error("Missing '.FIT' in header")
-        }
-
-        if (headerLength === 14) {
-            const crcHeader = blob[12] + (blob[13] << 8)
-            const crcHeaderCalc = calculateCRC(blob, 0, 12)
-            if (crcHeader !== crcHeaderCalc) {
-                logger.warn("FitParser.parse", "Header CRC mismatch", crcHeader, crcHeaderCalc)
-            }
-        }
-
-        const protocolVersion = blob[1]
-        const profileVersion = blob[2] + (blob[3] << 8)
-        const dataLength = blob[4] + (blob[5] << 8) + (blob[6] << 16) + (blob[7] << 24)
-        const crcStart = dataLength + headerLength
-        const crcFile = blob[crcStart] + (blob[crcStart + 1] << 8)
-        const crcFileCalc = calculateCRC(blob, headerLength === 12 ? 0 : headerLength, crcStart)
-
-        if (crcFile !== crcFileCalc) {
-            logger.warn("FitParser.parse", "File CRC mismatch", crcFile, crcFileCalc)
-        }
-
-        const fitObj: any = {}
-        fitObj.protocolVersion = protocolVersion
-        fitObj.profileVersion = profileVersion
-
-        const sessions = []
-        const workoutSteps = []
-        const laps = []
-        const events = []
-        const devices = []
-        const applications = []
-        const fieldDescriptions = []
-        const diveGases = []
-        const coursePoints = []
-        const sports = []
-        const monitors = []
-        const stress = []
-        const definitions = []
-        const fileIds = []
-        const monitorInfo = []
-        const lengths = []
-
-        let loopIndex = headerLength
-        const messageTypes = []
-        const developerFields = []
-
-        let startDate = void 0
-        let lastStopTimestamp = void 0
-        let pausedTime = 0
-
-        while (loopIndex < crcStart) {
-            const {nextIndex, messageType, message} = readRecord(blob, messageTypes, developerFields, loopIndex, options, startDate, pausedTime)
-            loopIndex = nextIndex
-
-            if (message) {
-                switch (messageType) {
-                    case "lap":
-                        laps.push(message)
-                        break
-                    case "session":
-                        sessions.push(message)
-                        break
-                    case "workout_step":
-                        workoutSteps.push(message)
-                        break
-                    case "event":
-                        if (message.event === "timer") {
-                            if (message.event_type === "stop_all") {
-                                lastStopTimestamp = message.timestamp
-                            } else if (message.event_type === "start" && lastStopTimestamp) {
-                                pausedTime += (message.timestamp - lastStopTimestamp) / 1000
-                            }
-                        }
-                        events.push(message)
-                        break
-                    case "length":
-                        lengths.push(message)
-                        break
-                    case "field_description":
-                        fieldDescriptions.push(message)
-                        break
-                    case "device_info":
-                        devices.push(message)
-                        if (!message["product_name"] && message["manufacturer"] && message["product"]) {
-                            const productNames = FIT.types.product[message["manufacturer"]]
-                            if (productNames && productNames[message["product"]]) {
-                                message["product_name"] = productNames[message["product"]]
-                            }
-                        }
-                        break
-                    case "developer_data_id":
-                        applications.push(message)
-                        break
-                    case "dive_gas":
-                        diveGases.push(message)
-                        break
-                    case "course_point":
-                        coursePoints.push(message)
-                        break
-                    case "sport":
-                        sports.push(message)
-                        break
-                    case "file_id":
-                        fileIds.push(message)
-                        break
-                    case "definition":
-                        definitions.push(message)
-                        break
-                    case "monitoring":
-                        monitors.push(message)
-                        break
-                    case "monitoring_info":
-                        monitorInfo.push(message)
-                        break
-                    case "stress_level":
-                        stress.push(message)
-                        break
-                    case "software":
-                        fitObj.software = message
-                        break
-                    default:
-                        if (messageType !== "") {
-                            fitObj[messageType] = message
-                        }
-                        break
-                }
-            }
-        }
-
-        fitObj.sessions = sessions
-        fitObj.workout_steps = workoutSteps
-        fitObj.laps = laps
-        fitObj.lengths = lengths
-        fitObj.events = events
-        fitObj.device_infos = devices
-        fitObj.developer_data_ids = applications
-        fitObj.field_descriptions = fieldDescriptions
-        fitObj.dive_gases = diveGases
-        fitObj.course_points = coursePoints
-        fitObj.sports = sports
-        fitObj.devices = devices
-        fitObj.monitors = monitors
-        fitObj.stress = stress
-        fitObj.file_ids = fileIds
-        fitObj.monitor_info = monitorInfo
-        fitObj.definitions = definitions
 
         // Extract duration and distance from sessions.
-        if (fitObj.sessions?.length > 0) {
-            fitFileActivity.distance = parseFloat((_.sumBy(fitObj.sessions, "total_distance") / 1000).toFixed(1))
-            fitFileActivity.totalTime = Math.round(_.sumBy(fitObj.sessions, "total_elapsed_time"))
+        if (messages.sessionMesgs?.length > 0) {
+            const sessions = messages.sessionMesgs
+            fitFileActivity.distance = parseFloat((_.sumBy(sessions, "totalDistance") / 1000).toFixed(1))
+            fitFileActivity.totalTime = Math.round(_.sumBy(sessions, "totalElapsedTime"))
 
             // Map our target activity fields to the FIT file fields.
             const fields = {
-                primaryBenefit: "primary_benefit",
-                intensityFactor: "intensity_factor",
-                tss: "training_stress_score",
-                trainingLoad: "training_load",
-                aerobicTrainingEffect: "total_training_effect",
-                anaerobicTrainingEffect: "total_anaerobic_effect",
-                pedalSmoothness: ["avg_combined_pedal_smoothness", "avg_left_pedal_smoothness", "avg_right_pedal_smoothness"],
-                pedalTorqueEffect: ["avg_left_torque_effectiveness", "avg_right_torque_effectiveness"],
-                pedalBalance: "left_right_balance"
+                primaryBenefit: "primaryBenefit",
+                intensityFactor: "intensityFactor",
+                tss: "trainingStressScore",
+                trainingLoad: "trainingLoadPeak",
+                aerobicTrainingEffect: "totalTrainingEffect",
+                anaerobicTrainingEffect: "totalAnaerobicTrainingEffect",
+                pedalSmoothness: ["avgCombinedPedalSmoothness", "avgLeftPedalSmoothness", "avgRightPedalSmoothness"],
+                pedalTorqueEffect: ["avgLeftTorqueEffectiveness", "avgRightTorqueEffectiveness"],
+                pedalBalance: "leftRightBalance"
             }
 
             // Append extra activity data from sessions.
-            for (let session of fitObj.sessions) {
+            for (let session of sessions) {
                 for (let field in fields) {
                     let fieldKey = fields[field]
                     let value: number
@@ -246,48 +87,32 @@ export class FitParser {
                     }
                 }
             }
-
-            // Get Sport profile.
-            for (let sp of fitObj.sports) {
-                if (!fitFileActivity.sportProfile && sp.name) {
-                    fitFileActivity.sportProfile = sp.name.replace(/\\u[\dA-F]{4}/gi, (unicode) => {
-                        return String.fromCharCode(parseInt(unicode.replace(/\\u/g, ""), 16))
-                    })
-                }
-            }
         }
 
-        // Round relevant fields.
-        for (let field of ["trainingLoad", "pedalSmoothness", "pedalTorqueEffect"]) {
-            if (fitFileActivity[field]) {
-                fitFileActivity[field] = Math.round(fitFileActivity[field])
-            }
+        // Get Sport profile.
+        if (messages.sportMesgs?.length > 0) {
+            fitFileActivity.sportProfile = messages.sportMesgs.map((sp) => (Array.isArray(sp.name) ? sp.name[0] : sp.name)).join(", ")
         }
 
         // Add workout details.
-        if (fitObj.workout?.wkt_name) {
-            fitFileActivity.workoutName = fitObj.workout.wkt_name
-        }
-
-        // Add workout notes.
-        if (fitObj.workout?.notes) {
-            fitFileActivity.workoutNotes = fitObj.workout.notes
+        if (messages.workoutMesgs?.length > 0) {
+            fitFileActivity.workoutName = messages.workoutMesgs.map((wk) => (Array.isArray(wk.wktName) ? wk.wktName[0] : wk.wktName)).join(", ")
+            fitFileActivity.workoutNotes = messages.workoutMesgs.map((wk) => (Array.isArray(wk.wktNotes) ? wk.wktNotes[0] : wk.wktNotes)).join(", ")
         }
 
         // Found devices in the FIT file? Generate device IDs.
-        if (fitObj.devices?.length > 0) {
-            const getDeviceString = (d) => `${d.manufacturer}.${d.product_name || d.device_type || d.source_type || d.device_index}.${d.serial_number}`.replace(/\_/g, "").replace(/\s/g, "")
-            const filter = (d) => d.manufacturer && d.serial_number
-            const validDevices = _.uniqBy(fitObj.devices.filter(filter), (d: any) => getDeviceString(d))
-            fitFileActivity.devices = validDevices.map((d) => getDeviceString(d))
+        if (messages.deviceInfoMesgs?.length > 0) {
+            const filter = (d) => d.manufacturer && d.serialNumber
+            const validDevices = _.uniqBy(messages.deviceInfoMesgs.filter(filter), (d: any) => this.getDeviceString(d))
+            fitFileActivity.devices = validDevices.map((d) => this.getDeviceString(d))
 
             // Identify devices battery statuses.
-            const batteryDevices = validDevices.filter((d) => d.battery_status)
+            const batteryDevices = validDevices.filter((d) => d.batteryStatus)
             if (batteryDevices.length > 0) {
                 fitFileActivity.deviceBattery = batteryDevices.map((d) => {
                     return {
-                        id: getDeviceString(d),
-                        status: d.battery_status
+                        id: this.getDeviceString(d),
+                        status: d.batteryStatus
                     }
                 })
             }
@@ -297,6 +122,13 @@ export class FitParser {
         if (fitFileActivity.primaryBenefit) {
             const primaryBenefits = ["None", "Recovery", "Base", "Tempo", "Threshold", "VO2Max", "Anaerobic", "Sprint"]
             fitFileActivity.primaryBenefit = primaryBenefits[fitFileActivity.primaryBenefit]
+        }
+
+        // Round relevant fields.
+        for (let field of ["trainingLoad", "pedalSmoothness", "pedalTorqueEffect"]) {
+            if (fitFileActivity[field]) {
+                fitFileActivity[field] = Math.round(fitFileActivity[field])
+            }
         }
 
         // Decode L/R balance, only right-power based calculation is supported for now.
@@ -309,12 +141,7 @@ export class FitParser {
             delete fitFileActivity.pedalBalance
         }
 
-        const logFields = Object.keys(fitFileActivity)
-        if (fitFileActivity.devices?.length > 0) {
-            logFields.push(...fitFileActivity.devices)
-        }
-
-        logger.info("FitParser.parse", logHelper.user(user), logHelper.fitFileActivity(fitFileActivity), `Data: ${logFields.join(", ")}`)
+        logger.info("FitParser.parse", logHelper.user(user), logHelper.fitFileActivity(fitFileActivity, true))
     }
 
     // DATABASE DATA
@@ -427,6 +254,18 @@ export class FitParser {
         } catch (ex) {
             logger.error("FitParser.saveProcessedActivity", logHelper.user(user), source, logHelper.fitFileActivity(activity), ex)
         }
+    }
+
+    // HELPERS
+    // --------------------------------------------------------------------------
+
+    /**
+     * Get a friendly device string based on the provided deviceInfo message.
+     * @param d The device info message.
+     */
+    private getDeviceString = (d) => {
+        const deviceName = d.garminProduct || d.faveroProduct || d.shimanoProduct || d.productName || d.antplusDeviceType || d.bleDeviceType || d.localDeviceType || d.sourceType
+        return `${d.manufacturer}.${deviceName}.${d.serialNumber}`.replace(/\_/g, "").replace(/\s/g, "").toLowerCase()
     }
 }
 
