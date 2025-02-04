e0d3d2e64da7cf9c61cef168ddb6ea97bcbdc9a5
Igor Ramadas <igor@devv.com>
2025-02-04 22:56:29 +0100

    Recipe action "GenerateInsights" does not need a value.

--- src/recipes/index.ts
+++ src/recipes/index.ts
@@ -194,7 +194,7 @@ export class Recipes {
                 }
 
                 // Some actions must have a value.
-                if (![RecipeActionType.Commute, RecipeActionType.GenerateDescription, RecipeActionType.GenerateName].includes(action.type)) {
+                if (![RecipeActionType.Commute, RecipeActionType.Trainer, RecipeActionType.GenerateDescription, RecipeActionType.GenerateName, RecipeActionType.GenerateInsights].includes(action.type)) {
                     if (action.value === null || action.value === "") {
                         throw new Error("Missing action value")
                     }
