8654ceaadc4bb61a7e687805e3f00475950c1c35
Igor Ramadas <igor@ramadas.net>
2025-10-02 20:39:17 +0200

    Updated to Claude Sonnet 4.5.

--- src/anthropic/index.ts
+++ src/anthropic/index.ts
@@ -68,7 +68,7 @@ export class Anthropic implements AiProvider {
                 method: "POST",
                 headers: {},
                 data: {
-                    model: user.isPro ? "claude-sonnet-4-0" : "claude-3-5-haiku-latest",
+                    model: user.isPro ? "claude-sonnet-4-5" : "claude-3-5-haiku-latest",
                     max_tokens: options.maxTokens,
                     system: options.instruction,
                     messages: [{role: "user", content: messages.join(" ")}]
